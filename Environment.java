39f6abd69a94dd0ef66557aa1d87800a45a6dd74
ignacioaris <arisignacio90@gmail.com>
2025-03-20 15:52:33 -0600

    Update Environment.java

--- Environment.java
+++ Environment.java
@@ -11,4 +11,29 @@ public Environment() {
     variablesStr = new HashMap<>();
     functions = new HashMap<>();
     }
+
+public void setVariable(String name, int value) {
+    variablesInt.put(name, value);
+    }
+
+public Integer getVariableInt(String name) {
+     return variablesInt.getOrDefault(name, 0);
+    }
+
+public void setVariable(String name, String value) {
+      variablesStr.put(name, value);
+    }
+
+public String getVariableStr(String name) {
+      return variablesStr.getOrDefault(name, "");
+    }
+
+public void defineFunction(String name, List<Token> body) {
+      functions.put(name, body);
+    }
+
+public List<Token> getFunction(String name) {
+      return functions.get(name);
+    }
+  
 }
