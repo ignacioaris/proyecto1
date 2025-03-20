3e5deae22c463955e0752f231c594900bc05c427
ignacioaris <arisignacio90@gmail.com>
2025-03-20 15:50:52 -0600

    Update Environment.java

--- Environment.java
+++ Environment.java
@@ -1,4 +1,14 @@
+import java.util.HashMap;
+import java.util.List;
+
 public class Environment{
+  private HashMap<String, Integer> variablesInt;
+  private HashMap<String, String> variablesStr;
+  private HashMap<String, List<Token>> functions;
 
-
+public Environment() {
+    variablesInt = new HashMap<>();
+    variablesStr = new HashMap<>();
+    functions = new HashMap<>();
+    }
 }
